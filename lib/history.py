918b7abb1c12e3ae66fa4851a161b29495701e1c
Giles Lavelle <giles.lavelle@gmail.com>
2012-04-15 22:09:47 +0100

    truncate the history grid when it's smaller than the user's terminal window

--- lib/history.py
+++ lib/history.py
@@ -2,6 +2,7 @@ from lib.settings import HISTORY_PATH
 from time import time
 from datetime import date
 from pyjson import PyJson
+import subprocess
 
 
 class History:
@@ -20,6 +21,7 @@ class History:
         - limit: the number of items to show
         - direction: the order to sort the files in to, (ascending or descending)
         - sort_by: the value to sort by (id, path, url or date)
+        - start: the record number to start at
         """
 
         history = sorted(self.history.doc['history'], key=lambda k: k[sort_by], reverse=direction == 'd')
@@ -28,6 +30,7 @@ class History:
         widths = [len(thing) for thing in header]
         grid = [header]
 
+        # iterate through the array of records, parse them and create a new 2d array of the formatted values
         for record in history[start - 1:]:
             if limit == 0:
                 break
@@ -39,6 +42,9 @@ class History:
 
             row = [_id, url, path, _date]
 
+            # find the longest cell in the column so the minimum width is known
+            # python's zip() function would be a cleaner way to do this than incrementing a variable
+            # but for some reason it didn't work as expected
             i = 0
             for thing in row:
                 length = len(thing)
@@ -51,24 +57,52 @@ class History:
             limit -= 1
 
         gridstring = '\n'
+        total = sum(widths)
+
+        # try to determine the width of the user's terminal window in characters
+        # so the rows can be truncated if they are too long
+        try:
+            terminal_width = int(subprocess.check_output('stty size').split()[1])
+        except:
+            print 'The size of your terminal window could not be determined'
+            print 'The layout of the grid below may be broken due to text wrapping'
+            terminal_width = 80  # default width for a lot of systems
+
+        # if the grid is wider than the terminal window work out the difference between them
+        # this is the amount to truncate the grid by
+        needs_truncating = False
+        if total > terminal_width:
+            terminal_diff = terminal_width - total - 12  # 12 is the number of dividers and spaces added
+            needs_truncating = True
+            widths[2] += terminal_diff  # reduce the width of the column by the difference
 
         for row in grid:
             i = 0
             rowstring = ''
 
             for cell in row:
+                # truncate this column if the table would be wider than the terminal window
+                # id, url and date don't vary much in width, only the local path is worth truncating
+                if i == 2 and needs_truncating and len(cell) > widths[2]:
+                    cell = cell[:terminal_diff - 3] + '...'
+
+                # pad with spaces so all cells in the column are the same width
                 diff = widths[i] - len(cell)
                 padding = ' ' * diff
                 cell = ' ' + cell + padding
-                rowstring += cell + ' |'
-                i += 1
-            rowstring = rowstring[:-1]
-            gridstring += rowstring + '\n'
 
+                rowstring += cell + ' |'  # add a divider between columns
+
+                i += 1
+
+            rowstring = rowstring[:-2]  # trim the last space and divider
+            gridstring += rowstring + '\n'  # add the row to the final grid
+
+            # add a horizontal divider only below the header
             if row == header:
                 gridstring += '-' * len(rowstring) + '\n'
 
-        print gridstring[:-1]
+        print gridstring[:-1]  # trim the last line break
 
     def add(self, path, url):
         """
