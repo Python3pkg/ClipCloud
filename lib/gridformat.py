6f1b135ef7c8b8a59cc91accf6aca94934ea1cd8
Giles Lavelle <giles.lavelle@gmail.com>
2012-04-18 16:52:35 +0100

    refactored grid formatter, including removal of temporary variables by using zip()

--- lib/gridformat.py
+++ lib/gridformat.py
@@ -8,22 +8,11 @@ def format_grid(grid):
     Returns: The grid as a nicely formatted string, with padding, dividers and
     truncation if it is wider than the terminal window.
     """
-    header = grid[0]
+    # find the longest cell in the column so the minimum width is known
+    widths = []
+    for y in [x for x in zip(*grid)]:
+        widths.append(max([len(z) for z in y]))
 
-    widths = [len(thing) for thing in header]
-
-    for row in grid:
-        # find the longest cell in the column so the minimum width is known
-        # python's zip() function would be a cleaner way to do this than incrementing a variable
-        # but for some reason it didn't work as expected
-        i = 0
-        for thing in row:
-            length = len(thing)
-            if length > widths[i]:
-                widths[i] = length
-            i += 1
-
-    gridstring = '\n'
     total = sum(widths)
 
     # try to determine the width of the user's terminal window in characters
@@ -37,36 +26,31 @@ def format_grid(grid):
 
     # if the grid is wider than the terminal window work out the difference between them
     # this is the amount to truncate the grid by
-    needs_truncating = False
+    terminal_diff = None
     if total > terminal_width:
         terminal_diff = terminal_width - total - 12  # 12 is the number of dividers and spaces added
-        needs_truncating = True
         widths[2] += terminal_diff  # reduce the width of the column by the difference
-
+    
+    a = []  # final grid array
     for row in grid:
-        i = 0
-        rowstring = ''
+        b = []  # final row array
 
-        for cell in row:
+        for cell, width in zip(row, widths):
             # truncate this column if the table would be wider than the terminal window
             # id, url and date don't vary much in width, only the local path is worth truncating
-            if i == 2 and needs_truncating and len(cell) > widths[2]:
+            if cell == row[2] and terminal_diff is not None and len(cell) > widths[2]:
                 cell = cell[:terminal_diff - 3] + '...'
 
             # pad with spaces so all cells in the column are the same width
-            diff = widths[i] - len(cell)
-            padding = ' ' * diff
-            cell = ' ' + cell + padding
+            cell += ' ' * (width - len(cell))
+            b.append(cell)
 
-            rowstring += cell + ' |'  # add a divider between columns
-
-            i += 1
-
-        rowstring = rowstring[:-2]  # trim the last space and divider
-        gridstring += rowstring + '\n'  # add the row to the final grid
+        r = ' ' + ' | '.join(b)  # join the columns into a string with a divider between
+        a.append(r)  # add the row to the final grid
 
         # add a horizontal divider only below the header
-        if row == header:
-            gridstring += '-' * len(rowstring) + '\n'
+        if row == grid[0]:
+            a.append('-' * len(r))
 
-    return gridstring[:-1]  # trim the last line break
+    grid = '\n' + '\n'.join(a)[:-1]  # convert to the final string and trim the last line break
+    return grid
